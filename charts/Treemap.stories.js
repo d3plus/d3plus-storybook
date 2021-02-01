940a2c81e21ae7230f9c08086607252bd0cf3cd7
Dave Landry <landry.dave@gmail.com>
2021-02-01 15:47:14 -0500

    cleans up initial Treemap story

--- charts/Treemap.stories.js
+++ charts/Treemap.stories.js
@@ -2,17 +2,14 @@ import React from 'react';
 import {Treemap} from './Treemap';
 
 export default {
-  title: "Treemap",
+  title: "Charts/Treemap",
   component: Treemap
-  // argTypes: {
-  //   backgroundColor: { control: 'color' }
-  // }
 };
 
 const Template = (args) => <Treemap config={args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const GettingStarted = Template.bind({});
+GettingStarted.args = {
   data: [
     {parent: "Group 1", id: "alpha", value: 29},
     {parent: "Group 1", id: "beta", value: 10},
@@ -20,22 +17,5 @@ Primary.args = {
     {parent: "Group 2", id: "delta", value: 29},
     {parent: "Group 2", id: "eta", value: 25}
   ],
-  groupBy: ["parent", "id"]
+  groupBy: "id"
 };
-
-// export const Secondary = Template.bind({});
-// Secondary.args = {
-//   label: 'Button',
-// };
-
-// export const Large = Template.bind({});
-// Large.args = {
-//   size: 'large',
-//   label: 'Button',
-// };
-
-// export const Small = Template.bind({});
-// Small.args = {
-//   size: 'small',
-//   label: 'Button',
-// };
