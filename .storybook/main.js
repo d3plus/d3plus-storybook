25a651550ee2f4f2084bd2d1b7929b4d19d4bb2a
Dave Landry <landry.dave@gmail.com>
2021-02-01 15:47:01 -0500

    adds initial theming

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,7 +8,14 @@ module.exports = {
   ],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        actions: false,
+        backgrounds: false,
+        viewport: false
+      }
+    }
   ],
   webpackFinal: async (config) => {
 
---
+++ .storybook/manager.js
@@ -0,0 +1,7 @@
+import {addons} from "@storybook/addons";
+import theme from "./theme.js";
+
+addons.setConfig({
+  isToolshown: false,
+  theme
+});
