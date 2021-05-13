f155776ec4e09fd200e24840804aa97b2e8c44f4
Dave Landry <landry.dave@gmail.com>
2021-05-13 00:12:09 -0400

    finishes Viz primitive

--- charts/Treemap.args.js
+++ charts/Treemap.args.js
@@ -5,9 +5,18 @@ import {Treemap as D3plusTreemap} from "d3plus-react";
 export const Treemap = ({config}) => <D3plusTreemap config={config} />;
 
 export const argTypes = {
+
+  /**
+   * Filters out unused argTypes from the Viz primitive and
+   * overrides any defaults that have been changed in Treemap
+   */
   ...Object.keys(Viz.argTypes)
-    .filter(k => !k.match(/^zoom*/))
+    .filter(k => !k.match(/^(discrete|shape|zoom.*)$/))
     .reduce((obj, k) => (obj[k] = Viz.argTypes[k], obj), {}),
+
+  /**
+   * Treemap-specific methods
+   */
   sum: {
     type: {
       summary: "string | function",
--- primitives/BaseClass.js
+++ primitives/BaseClass.js
@@ -4,6 +4,10 @@ const SampleClass = new BaseClass();
 
 export default {
   argTypes: {
+
+    /**
+     * category: Formatting
+     */
     locale: {
       type: {
         summary: "object | string"
@@ -25,11 +29,16 @@ The locale passed to this method can either be a locale \`object\` (internal exa
 
 Both 4-character and 2-character codes are accepted (ex. "en" becomes "en-US").`
     },
+
+    /**
+     * category: User Interaction
+     */
     on: {
       type: {
         summary: "object"
       },
       table: {
+        category: "User Interaction",
         defaultValue: {
           summary: "{}"
         }
@@ -40,4 +49,5 @@ Both 4-character and 2-character codes are accepted (ex. "en" becomes "en-US").`
 As with most Array accessor functions, the event \`function\` provided receives the current data point (\`d\`) and index (\`i\`) of that data point in the array as it's 2 arguments.`
     }
   }
+
 }
