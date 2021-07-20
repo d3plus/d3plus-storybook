b6d8df12ce4b51a4c8635162e1abb1c2dc5bedf9
ffigueroal <felipe.figueroa.lara@gmail.com>
2021-07-20 09:52:14 -0400

    Revert "Merge pull request #8 from d3plus/treemap-examples"
    This reverts commit 71d27b6443d2c1a807bd1f1ffab20475396e5e1f, reversing
    changes made to 650abb96e8b045a7dc056110921c3788cbd6f3d1.

--- charts/Treemap.stories.js
+++ charts/Treemap.stories.js
@@ -35,28 +35,3 @@ NestedData.args = {
   groupBy: ["parent", "id"],
   sum: "value"
 };
-
-export const DifferentTilingMethods = Template.bind({});
-DifferentTilingMethods.args = {
-  data: [
-    {"Group": "Store", "Sub-Group": "Convenience Store", "Number of Stores": 100},
-    {"Group": "Store", "Sub-Group": "Grocery Store", "Number of Food Stores": 150},
-    {"Group": "Store", "Sub-Group": "Supercenters", "Number of Food Stores": 30},
-    {"Group": "Other", "Sub-Group": "Farmer's Market", "Number of Food Stores": 50},
-    {"Group": "Restaurant", "Sub-Group": "Fast-Food Restaurant", "Number of Food Stores": 60},
-    {"Group": "Restaurant", "Sub-Group": "Full-Service Restaurant", "Number of Food Stores": 120}
-  ],
-  groupBy: ["Group", "Sub-Group"],
-  sum: "Number of Food Stores",
-  tile: "slice"
-};
-
-export const SmallDataThreshold = Template.bind({});
-SmallDataThreshold.args = {
-  colorScale: "Population",
-  data: "https://datausa.io/api/data?measures=Population&drilldowns=State&Year=2018",
-  groupBy: "State",
-  sum: "Population",
-  threshold: 0.0025,
-  thresholdName: "States"
-};
