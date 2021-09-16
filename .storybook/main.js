76b64b7ac16946cf07e5ae95bbb90917e824b85f
Dave Landry <landry.dave@gmail.com>
2021-09-16 17:10:56 -0400

    removes now unecessary CSS loaders from dependencies and build

--- .storybook/main.js
+++ .storybook/main.js
@@ -21,16 +21,5 @@ module.exports = {
         viewport: false
       }
     }
-  ],
-  webpackFinal: async (config) => {
-
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-      include: path.resolve(__dirname, "../"),
-    });
-
-    return config;
-
-  }
+  ]
 };
