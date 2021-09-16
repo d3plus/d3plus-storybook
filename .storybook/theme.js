997ac5bf3182d0719bed5576e31cecaa3e118327
Dave Landry <landry.dave@gmail.com>
2021-09-16 17:13:32 -0400

    removes unused properties from theme.js

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -5,31 +5,11 @@ export default create({
   colorPrimary: "#6A994E",
   colorSecondary: "#6A994E",
 
-//   // UI
-//   appBg: 'white',
-//   appContentBg: 'silver',
-//   appBorderColor: 'grey',
-//   appBorderRadius: 4,
-
-//   // Typography
+  // Typography
   fontBase: "'Roboto', 'Helvetica Neue', 'HelveticaNeue', 'Helvetica', 'Arial', sans-serif",
   fontCode: "'Roboto Mono', monospace",
 
-//   // Text colors
-//   textColor: 'black',
-//   textInverseColor: 'rgba(255,255,255,0.9)',
-
-//   // Toolbar default and active colors
-//   barTextColor: 'silver',
-//   barSelectedColor: 'black',
-//   barBg: 'hotpink',
-
-//   // Form colors
-//   inputBg: 'white',
-//   inputBorder: 'silver',
-//   inputTextColor: 'black',
-//   inputBorderRadius: 4,
-
   brandTitle: "<span class=\"logo-d3\">D3</span>plus",
   brandUrl: "https://d3plus.org"
+
 });
