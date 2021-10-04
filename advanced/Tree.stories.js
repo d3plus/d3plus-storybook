8ea379e6fc48c32ffcb79218e50860ca5d757c1d
ffigueroal <felipe.figueroa.lara@gmail.com>
2021-10-04 11:37:29 -0300

    restore hidden tree example

--- advanced/Tree.stories.js
+++ advanced/Tree.stories.js
@@ -10,9 +10,16 @@ export default {
 
 const Template = (args) => <Tree config={configify(args, argTypes)} />;
 
-/**
- export const GettingStarted = Template.bind({});
- GettingStarted.args = {
-
+/* export const GettingStarted = Template.bind({});
+GettingStarted.args = {
+ data: [
+   {parent: "Group 1", id: "alpha", value: 29},
+   {parent: "Group 1", id: "beta", value: 10},
+   {parent: "Group 1", id: "gamma", value: 2},
+   {parent: "Group 2", id: "delta", value: 29},
+   {parent: "Group 2", id: "eta", value: 25}
+ ],
+ groupBy: ["parent", "id"],
+ orient: "horizontal"
 };
 */
